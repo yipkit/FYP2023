9fdbe7ce22df7ed3aef83207d16e112fae7e4e29
yipkit <yipkit0830@gmail.com>
2023-01-27 12:34:57 +0800

    New Http

---
+++ send_email.cs
@@ -0,0 +1,66 @@
+using System;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace mail_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MimeMessage message = new MimeMessage();
+            //send from our account
+            message.From.Add(new MailboxAddress("testing", "xxx@gmail.com"));
+            //send to customer
+            message.To.Add(MailboxAddress.Parse("xxx@gmail.com"));
+
+            message.Subject = "testing";
+            message.Body = new TextPart("plain")
+            {
+
+                Text = @"This is testing!!"
+
+            };
+
+            Console.Write("Email : ");
+            string emailAddress = Console.ReadLine();
+            Console.Write("Password : ");
+
+            ConsoleColor originalBGColor = Console.BackgroundColor;
+            ConsoleColor originalFGColor = Console.ForegroundColor;
+
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            string password = Console.ReadLine();
+
+            Console.BackgroundColor = originalBGColor;
+            Console.ForegroundColor = originalFGColor;
+
+            SmtpClient client = new SmtpClient();
+            try
+            {
+
+                client.Connect("smtp.gmail.com", 465, true);
+                client.Authenticate(emailAddress, password);
+                client.Send(message);
+                Console.WriteLine("Email Sent!!");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                client.Disconnect(true);
+                client.Dispose();
+
+            }
+        }
+    }
+}
